f27ad2bb23d6c289a90910cdc3ee1f9ae1ac6ca6
tuanio <nvatuan3@gmail.com>
2021-11-19 17:03:54 +0700

    update the version of multi threading for python files

--- isolated_word_recognition/digit_speech_recognition.py
+++ isolated_word_recognition/digit_speech_recognition.py
@@ -2,48 +2,52 @@
 # coding: utf-8
 import glob
 import numpy as np
+import seaborn as sns
 from hmmlearn import hmm
 from scipy.io import wavfile
+import matplotlib.pyplot as plt
 from collections import defaultdict
 from python_speech_features import mfcc
-
+import concurrent.futures as cf
 
 data_path = glob.glob('datasets/digits/*')
+
+sr, _ = wavfile.read(data_path[0])
+
+labels = np.arange(10).tolist()
+
+db = defaultdict(list)
+for path in data_path:
+    label = int(path.split('_', 1)[0][-1])
+    _, signal = wavfile.read(path)
+    db[label].append(signal)
 
 
 def feature_analysis(x):
     return mfcc(x, sr, winlen=0.025, winstep=0.01, preemph=0.95, winfunc=np.hamming)
 
 
-data = defaultdict(list)
-for path in data_path:
-    sr, signal = wavfile.read(path)
-    # get features from speech signal
-    signal = feature_analysis(signal)
-    label = path.split('\\')[-1].split('_', 1)[0]
-    data[int(label)].append(signal)
+def get_list_mfcc(label):
+    with cf.ThreadPoolExecutor() as executor:
+        data = executor.map(feature_analysis, db[label])
+    return list(data)
 
 
-train = dict()
-test = dict()
+with cf.ThreadPoolExecutor() as executor:
+    data = executor.map(get_list_mfcc, labels)
+    data = dict(zip(labels, data))
+
 
 all_size = len(data[0])
 
 train_size = int(0.8 * all_size)
-test_size = int(all_size - train_size)
+test_size = all_size - train_size
 
-print(train_size, test_size)
-
-for label in data.keys():
-    train[label] = np.array([])
-    test[label] = list()
-    for feature in data[label][:train_size]:
-        if train[label].shape[0] == 0:
-            train[label] = feature
-        else:
-            train[label] = np.append(train[label], feature, axis=0)
-    for feature in data[label][-test_size:]:
-        test[label].append(feature)
+train = dict()
+test = dict()
+for label in labels:
+    train[label] = np.concatenate(data[label][:train_size], axis=0)
+    test[label] = data[label][-test_size:]
 
 
 def loglh(model, observation):
@@ -53,22 +57,31 @@ def loglh(model, observation):
 
 def predict_speech(observation):
     loglikelihood = [loglh(model, observation)
-                     for model in speech_model.values()]
+                     for model in speech_models.values()]
     return np.argmax(loglikelihood)
 
 
-speech_model = dict()
-for label in np.arange(10):
-    speech_model[label] = hmm.GMMHMM(
-        n_components=2, n_mix=2, n_iter=1000, verbose=False)
-    speech_model[label].fit(train[label])
+def train_hmm(label):
+    model = hmm.GMMHMM(n_components=2, n_mix=2, n_iter=1000, verbose=False)
+    model.fit(train[label])
+    return model
 
+
+with cf.ThreadPoolExecutor() as executor:
+    speech_models = executor.map(train_hmm, labels, chunksize=3)
+    speech_models = dict(zip(labels, speech_models))
 
 true = 0
 test_data_size = 0
+confusion_matrix = np.zeros(shape=(10, 10))
 for label in range(10):
     for observation in test[label]:
         pred = predict_speech(observation)
         true += pred == label
+        confusion_matrix[label, pred] += 1
         test_data_size += 1
 print("Accuracy: %.2f" % (true / test_data_size))
+fig, ax = plt.subplots()
+sns.heatmap(confusion_matrix, annot=True, fmt='.0f', ax=ax)
+# ax.set_xlabel('')
+plt.savefig('confusion_matrix.png')
--- isolated_word_recognition/fruits_speech_recognition.py
+++ isolated_word_recognition/fruits_speech_recognition.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 import glob
+import time
 import numpy as np
 from hmmlearn import hmm
 from scipy.io import wavfile
 from collections import defaultdict
 from python_speech_features import mfcc
+import concurrent.futures as cf
+
+start_time = time.perf_counter()
 
 db = defaultdict(list)
 
@@ -15,6 +19,7 @@ for path in glob.glob('datasets/fruits/*'):
         sr, signal = wavfile.read(wav_path)
         db[label].append(signal.astype('float'))
 
+labels = db.keys()
 
 # get features
 # 1. pre-emphasis with 0.97
@@ -22,11 +27,19 @@ for path in glob.glob('datasets/fruits/*'):
 # 3. apply Hamming window function
 # 4. FFT
 # 5. Mel frequency cepstrum
-features = defaultdict(list)
-for label in db.keys():
-    features[label] = list(
-        map(lambda x: mfcc(x, sr, winfunc=np.hamming), db[label]))
 
+def get_mfcc(x):
+    return mfcc(x, sr, winfunc=np.hamming)
+
+def get_list_mfcc(label):
+    with cf.ThreadPoolExecutor() as executor:
+        results = executor.map(get_mfcc, db[label])
+    return list(results)
+
+
+with cf.ThreadPoolExecutor() as executor:
+    features = executor.map(get_list_mfcc, labels)
+    features = dict(zip(labels, features))
 
 train = dict()
 test = dict()
@@ -43,20 +56,22 @@ for label in features.keys():
         test[label].append(feature)
 
 
-models = dict()
-for label in features.keys():
-    models[label] = hmm.GaussianHMM(n_components=5, n_iter=100)
-    models[label].fit(train[label])
+def train_hmm(label):
+    model = hmm.GaussianHMM(n_components=5, n_iter=100)
+    model.fit(train[label])
+    return model
 
+with cf.ThreadPoolExecutor() as executor:
+    models = executor.map(train_hmm, labels, chunksize=10)
+    models = dict(zip(labels, models))
 
 def predict_speech(sample):
     def get_ll(model):
         ll, hidden = model.decode(sample)
         return ll
-    predicted_ll = [get_ll(model) for label, model in models.items()]
-    labels = list(models.keys())
+    predicted_ll = [get_ll(model) for model in models.values()]
     idx = np.argmax(predicted_ll)
-    return labels[idx]
+    return list(labels)[idx]
 
 len_data = 0
 right_predicted = 0
@@ -65,5 +80,4 @@ for label, data in test.items():
         pred = predict_speech(datum)
         len_data += 1
         right_predicted += pred == label
-#         print("Actual: {}, Predicted: {}".format(label, pred))
 print("Accuracy: %.2f" % (right_predicted / len_data))
